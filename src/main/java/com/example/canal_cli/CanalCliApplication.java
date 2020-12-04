637d8110433ee4cd7b019c7e6fcf2bcf8c436782
pengzeyuan <583119263@qq.com>
2020-12-04 16:11:08 +0800

    first commit

---
+++ src/main/java/com/example/canal_cli/CanalCliApplication.java
@@ -0,0 +1,14 @@
+package com.example.canal_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanalCliApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CanalCliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/canal_cli/CanalCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.canal_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanalCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
